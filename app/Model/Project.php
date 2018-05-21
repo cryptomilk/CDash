e017b4c39a705cbfe8ea5c78883ab9bc43b11042
Bryon Bean <bryon.bean@kitware.com>
2018-05-21 13:28:53 -0400

    fix: fixes instantiation of subscriber collection, adds defaults for mail

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -135,6 +135,12 @@ class Project
         if (empty($this->WebApiKey)) {
             $this->WebApiKey = '';
         }
+        if (empty($this->EmailMaxItems)) {
+            $this->EmailMaxItems = 5;
+        }
+        if (empty($this->EmailMaxChars)) {
+            $this->EmailMaxChars = 255;
+        }
         $this->PDO = Database::getInstance()->getPdo();
     }
 
@@ -1671,7 +1677,7 @@ class Project
     public function GetSubscriberCollection()
     {
         if (!$this->SubscriberCollection) {
-            $this->SubscriberCollection = new SubscriberCollection();
+            $this->SubscriberCollection = $this->GetProjectSubscribers();
         }
 
         return $this->SubscriberCollection;
@@ -1685,7 +1691,7 @@ class Project
     public function GetProjectSubscribers()
     {
         $service = ServiceContainer::getInstance()->getContainer();
-        $collection = $this->GetSubscriberCollection();
+        $collection = $service->make(SubscriberCollection::class);
         $sql = 'SELECT
               u2p.*,
               user.email email
